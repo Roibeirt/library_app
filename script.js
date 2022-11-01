9e64d67127353bd15c775bac115dd678a55ef71b
Bobby Aston <bobby.j.aston@gmail.com>
2022-11-01 19:44:12 +0900

    Finished basic functionality and styling

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+/* Declare elements */
+/* Header elements */
+const openFormButton = document.getElementById("open-form-button");
+/* Form elements */
 const inputForm = document.getElementById("input-form");
 const inputTitle = inputForm.elements.inputTitle;
 const inputAuthor = inputForm.elements.inputAuthor;
 const inputPages = inputForm.elements.inputPages;
 const inputRead = inputForm.elements.inputRead;
 const submitButton = inputForm.elements.inputSubmit;
-const booksTable = document.getElementById("booksTable");
+const forms = document.getElementById("forms");
+const formCloseButton = document.getElementById("add-book-close");
+/* Body elements */
+const contentArea = document.getElementById("content-area");
+/* Card Elements */
+const card = document.getElementsByClassName("card");
 
-let test = [];
-test.push(inputForm, inputTitle);
+/* Form should be hidden by default, so remove it when loading the page */
+removeBookForm();
+
+/* Initialize library array for book objects */
 let myLibrary = [];
 
+/* Declare book object */
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -17,15 +29,24 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-myLibrary.push(new Book("The Hobbit", "JRR Tolkein", "350", "Read"));
+/* Add listener to button that opens input form */
+openFormButton.addEventListener("click", () => displayBookForm() );
 
-populateTable();
-
+/* Add book to library from form inputs */
 function addBookToLibrary(){
 
+    
+
+    /* Get user input */
     let title = inputTitle.value;
     let author = inputAuthor.value;
     let pages = inputPages.value;
+
+    /* We are bypassing submit button by writing our own submit functions, so need to validate input field ourself */
+    if(title.trim() == "" || author.trim() == "" || pages.trim() == ""){
+        return(console.log("required fields not filled out"));
+    }
+
     let read;
     if(inputRead.checked == true){
         read = "Read";
@@ -34,42 +55,125 @@ function addBookToLibrary(){
         read = "Unread";
     }
 
+    /* Call book constructor and pass user input */
     const newBook = new Book(title, author, pages, read);
+    /* Push book to library */
     myLibrary.push(newBook);
 
+    /* Reset user input fields to null */
     inputTitle.value = null;
     inputAuthor.value = null;
     inputPages.value = null;
     inputRead.checked = false;
 
-    populateTable();
+    /* Call populate cards to add new book object to screen */
+    populateCards();
+
+    /* Remove form from page */
+    removeBookForm();
+
 
 }
 
-submitButton.addEventListener("click", () => {addBookToLibrary();});
+/* Add listener to form submit button*/
+submitButton.addEventListener("click", (event) => {
+    /* not using preventDefault() as I still want it to validate fields
+    event.preventDefault();*/
+    addBookToLibrary()
+});
 
-function populateTable(){
+/* Function to display input form */
+function displayBookForm(){
 
-    clearTable();
+    contentArea.appendChild(forms);
+
+}
+
+/* Function to hide input form */
+function removeBookForm(){
+
+    forms.remove();
+
+}
+
+/* sets close button on form to close form */
+formCloseButton.addEventListener("click", () => removeBookForm());
+
+/* function to create new card */
+function createCard(title, author, pages, read, cardIndex){
+
+    /* add card info with same styling to array so we can loop creation of these elements and save some typing*/
+    let textInfo = [title, author, pages];
+
+    /* create main card container and assign class */
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    /* create delete button */
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-card-button");
+    let buttonIcon = document.createElement("span");
+    buttonIcon.classList.add("material-icons");
+    let iconIdentifier = document.createTextNode("close");
+    buttonIcon.appendChild(iconIdentifier);
+    deleteButton.appendChild(buttonIcon);
+    card.appendChild(deleteButton);
+    /* delete button should remove book from object array and delete its own card */
+    deleteButton.addEventListener("click", () => {
+        myLibrary.splice(cardIndex,1);
+        card.remove();
+    });
+
+    /* loop through creation of elements for info in textInfo array */
+    for(text of textInfo){
+        let cardInfo = document.createElement("div");
+        cardInfo.classList.add("card-info")
+        let cardText = document.createTextNode(text)
+        cardInfo.appendChild(cardText);
+        card.appendChild(cardInfo);
+    }
+    /* create read element */
+    let cardRead = document.createElement("button");
+    cardRead.classList.add("card-read");
+    let readText = document.createTextNode(read);
+    cardRead.appendChild(readText);
+    /* assign function for changing read / unread on button */
+    cardRead.addEventListener("click", () => readUnread(cardIndex));
+    card.appendChild(cardRead);
+    
+    /* append card to page */
+    contentArea.appendChild(card);
+}
+
+/* find all card elements on page and remove them */
+function clearCards(){
+
+    let cardList = document.querySelectorAll(".card");
+    cardList.forEach(card => card.remove());
+
+}
+
+/* firstly clear cards so we don't create duplicates, then call cardCreate on every object in myLibrary */
+function populateCards(){
+
+    clearCards();
     
     for (x=0; x<myLibrary.length; x++){
-        let newRow = document.createElement("tr");
-        for (y of Object.values(myLibrary[x])){
-            let newCell = document.createElement("td")
-            cellText = document.createTextNode(y);
-            newCell.appendChild(cellText);
-            newCell.classList.add("table-data");
-            newRow.appendChild(newCell);
-        }
-        newRow.classList.add("table-row");
-        booksTable.appendChild(newRow);
-    }
+        
+        createCard(myLibrary[x].title, myLibrary[x].author, myLibrary[x].pages, myLibrary[x].read, x);
 
+    }
 
 }
 
-function clearTable(){
-    const tableRows = document.querySelectorAll(".table-row");
-    tableRows.forEach(row => row.remove());
+/*check book object referenced by index, if read, change unread and repopulate (or vice versa)*/
+function readUnread(index){
+    if (myLibrary[index].read =="Read"){
+        myLibrary[index].read = "Unread";
+    }
+    else{
+        myLibrary[index].read = "Read";
+    }
 
+    populateCards();
 }
